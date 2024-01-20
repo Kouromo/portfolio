2e3e3e43cf6cf424aeb27308aed58ae39e5c0c37
Matéo Alves <blackwolf4728@gmail.com>
2024-01-20 18:38:44 +0100

    More layout

---
+++ src.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('show');
+
+    // Ajout de cette ligne pour cacher le menu lors d'un deuxième clic
+    if (!menu.classList.contains('show')) {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'flex';
+    }
+}
